c4277b006eab4d6137bf8c2a4dc847bf86ea5b6a
Zyhar Zakhar <93952085+zzyhar@users.noreply.github.com>
2024-05-08 10:22:04 -0400

    Adding comment, fixing layout, and styling for header responsiveness

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -27,10 +27,9 @@ function Header() {
           <img src={people} alt="people" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-
-        <div className="gpt3__header-image">
-          <img src={ai} alt="AI Face" />
-        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="AI Face" />
       </div>
     </div>
   );
